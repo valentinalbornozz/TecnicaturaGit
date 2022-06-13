0c4d0b42c590253edd05b280d9968defe012bd45
Valentin Albornoz <valeg486@gmail.com>
2022-06-13 01:51:45 -0300

    Actualización Java

--- Java/Leccion1/HolaMundo.java
+++ Java/Leccion1/HolaMundo.java
@@ -1,11 +1,7 @@
 package Leccion1;
 
-import java.util.Scanner;
-
-import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 public class HolaMundo {
-    private static final Scanner entrada = null;
 
     public static void main(String[] args) {
         /*
@@ -153,7 +149,7 @@ public class HolaMundo {
         System.out.println("entrada = " + entrada);*/
 
         //Conversión de tipos primitivos en Java Parte 2
-        var edadTexto = String.valueOf(10);
+        /*var edadTexto = String.valueOf(10);
         System.out.println("edadTexto = " + edadTexto);
 
         var fraseChar = "Programadores".charAt(4);
@@ -161,6 +157,47 @@ public class HolaMundo {
 
         System.out.println("Digite un caracter: ");
         fraseChar = entrada.nextLine().charAt(0);
-        System.out.println("fraseChar = " + fraseChar);
+        System.out.println("fraseChar = " + frase);*/
+
+        /*int num1 = 5; int num2 = 4;
+        var solucion = num1 + num2;
+        System.out.println("Solución de la suma = " + solucion);
+
+        solucion = num1 - num2;
+        System.out.println("solucion de la resta = " + solucion);
+
+        solucion = num1 * num2;
+        System.out.println("solución de la multiplicación = " + solucion);
+
+        solucion = num1 / num2;
+        System.out.println("solución de la división = " + solucion);
+
+        var solucion2 = 3.4 /num2;
+        System.out.println("solucion2 resultado de la división = " + solucion);
+
+        solucion = num1 % num2; //Guarda el residuo entero de la división
+        System.out.println("solución = " + solucion); // 5/4
+
+        if(num2 %2 == 0)
+            System.out.println("Es un número Par");
+        else
+            System.out.println("Es un número Impar");*/
+
+        int varNum1 = 1, varNum2 = 4;;
+        var varNum3 = varNum1 + 6 - varNum2; //Una operación
+        System.out.println("varNum3 = " + varNum3);
+
+        varNum1 += 1;
+        System.out.println("varNum1 = " + varNum1);
+
+//      -=  *=  /=  %=
+        varNum2 -= 2;
+        System.out.println("varNum2 = " + varNum2);
+        varNum1 *= 5;
+        System.out.println("varNum1 = " + varNum1); 
+        varNum3 /= 4;
+        System.out.println("varNum3 = " + varNum3);
+        varNum1 %= 6;
+        System.out.println("varNum1 = " + varNum1);
     }
 }
