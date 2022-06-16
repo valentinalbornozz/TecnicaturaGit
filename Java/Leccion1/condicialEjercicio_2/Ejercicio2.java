c25a9fb6912f6ed9b4924588154a5e897a3bae54
Valentin Albornoz <valeg486@gmail.com>
2022-06-16 17:38:33 -0300

    Actualzación Java ejercicios clase 10

---
+++ Java/Leccion1/condicialEjercicio_2/Ejercicio2.java
@@ -0,0 +1,29 @@
+package Leccion1.condicialEjercicio_2;
+
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite un número de mes: ");
+        var mes = Integer.parseInt(entrada.nextLine());
+        var estacion = "Estación desconocida";
+        switch (mes) {
+            case 1: case 2: case 3:
+                estacion = "Verano";
+                break;
+            case 4: case 5: case 6:
+                estacion = "Otinio";
+                break;
+            case 7: case 8: case 9:
+                estacion = "Invierno";
+                break;
+            case 10: case 11: case 12:
+                estacion = "Primavera";
+                break;
+        }
+        System.out.println("estacion = " + estacion);
+    }
+}
