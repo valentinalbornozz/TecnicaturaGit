de4a2b2674d522b9d068be05a797970c03f539c4
Valentin Albornoz <valeg486@gmail.com>
2022-08-29 19:22:58 -0300

    actualizacion ejerciciowhile01

--- Java/Leccion2/CicloWhile/EjercicioWhile01.java
+++ Java/Leccion2/CicloWhile/EjercicioWhile01.java
@@ -1,8 +1,8 @@
-package CicloWhile;
+
 
 public class EjercicioWhile01 {
     public static void main(String[] args) {
-        var conteo = 0 //Inferencia de tipos
+        var conteo = 0; //Inferencia de tipos
         while(conteo <= 7){
             System.out.println("conteo = " + conteo);
             conteo++; // Vamos aumentando en uno la variable
