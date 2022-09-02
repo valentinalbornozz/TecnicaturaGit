55c1747a6096eb23a1d39af17b24418769b83797
Valentin Albornoz <valeg486@gmail.com>
2022-09-02 20:19:33 -0300

    Ingresnado las clases de colecciones en python y ejercicio en java

---
+++ Java/Leccion2/EjercicioCiclos01/ciclos01/Ejercicio01.java
@@ -0,0 +1,18 @@
+package Leccion2.EjercicioCiclos01.ciclos01;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        int numero, cuadrado;
+        
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número: "));
+        while(numero >= 0) {
+            cuadrado = (int)Math.pow(numero,2);
+            System.out.println("El numero "+numero+" elevado al cuadrado es: "+cuadrado);
+            System.out.println("Digite otro numero: ");
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número: "));
+        }
+        System.out.println("El programa a finalizado por numero negativo");
+    }
+}
---
+++ Java/Leccion2/EjercicioCiclos02/ciclos02/Ejercicio02.java
@@ -0,0 +1,22 @@
+package Leccion2.EjercicioCiclos02.ciclos02;
+
+import java.util.Scanner;
+
+public class Ejercicio02 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite un número: ");
+        var numero = Integer.parseInt(entrada.nextLine());
+        while(numero != 0){
+            if(numero > 0){
+                System.out.println("El número "+numero+" es POSITIVO");
+            }
+            else {
+                System.out.println("El número "+numero+" es NEGATIVO");
+            }
+            System.out.println("Digite otro número: ");
+            numero = Integer.parseInt(entrada.nextLine());
+        }
+        System.out.println("El numero "+numero+" finaliza el programa");
+    }
+}
