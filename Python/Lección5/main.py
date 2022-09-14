b8116398476126fe68562276019d02edcb34147a
Valentin Albornoz <valeg486@gmail.com>
2022-09-13 22:24:03 -0300

    Arreglamos la ubicacion de los ejercicios y agregamos leccion5

---
+++ Python/Lección5/main.py
@@ -0,0 +1,8 @@
+# Comenzamos con Funciones
+# mi_funcion() # No se puede llamar antes de definir una función 
+# Definimos una función
+def mi_funcion(): # Para identificar a la función utilizamos paréntesis
+    print('Saludos a todos lo alumnos de la Tecnicatura')
+
+mi_funcion() # Estamos llamando a la función
+mi_funcion() # Se puede llamar a una función N cantidad de veces
