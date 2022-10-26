86a2909ab9f3f2605ced2d1ed38d8f7921929c63
Valentin Albornoz <valeg486@gmail.com>
2022-10-25 21:54:54 -0300

    commiteamos Clase-7-Js

---
+++ JavaScript/leccion03/03-01-OperadoresJavaScript-ujs.js
@@ -0,0 +1,18 @@
+// EJercicio para enocntra números pares
+
+let parInpar = 1;
+if(parInpar % 2 == 0) {
+    console.log("Es un número PAR");
+}
+else {
+    console.log("Es un número IMPAR");
+}
+
+// Ejercicio: es mayor de edad
+let edad = 18, adulto = 18;
+if (edad >= adulto){
+    console.log('Usted es una persona adulta');
+}
+else{
+    console.log('Usted es una persona menor de edad');
+}
