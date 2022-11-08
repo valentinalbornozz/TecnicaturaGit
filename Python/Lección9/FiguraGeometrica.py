4f1e85c2ed932c69fde98f0551be0aa5e45e50dc
Valentin Albornoz <valeg486@gmail.com>
2022-11-08 20:40:24 -0300

    clase-13-abstract

--- Python/Lección9/FiguraGeometrica.py
+++ Python/Lección9/FiguraGeometrica.py
@@ -1,4 +1,9 @@
-class FiguraGeometrica:
+from abc import ABC, abstractmethod
+
+
+# ABC significa: Abstract Base Class, convierte una clase en abstracta
+
+class FiguraGeometrica(ABC):
     def __init__(self, ancho, alto):
         if self._validar_valores(ancho):
             self._ancho = ancho
@@ -19,6 +24,8 @@ class FiguraGeometrica:
     def ancho(self, ancho):
         if self._validar_valores(ancho):
             self._ancho = ancho
+        else:
+            print(f'valor erroneo ancho: {ancho}')
 
     @property
     def alto(self):
@@ -28,9 +35,15 @@ class FiguraGeometrica:
     def alto(self, alto):
         if self._validar_valores(alto):
             self._alto = alto
+        else:
+            print(f'valor erroneo alto: {alto}')
+
+    @abstractmethod
+    def calcular_area(self):
+        pass
 
     def __str__(self):
         return f'FiguraGeometrica [Ancho: {self._ancho}, Alto: {self._alto}]'
 
-    def _validar_valores(self, valor): # Método encapsulado
+    def _validar_valores(self, valor):  # Método encapsulado
         return True if 0 < valor < 10 else False
